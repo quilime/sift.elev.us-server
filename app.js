3e19fba1e4be0cf5a1619cd8b40174b6c71fc0e5
Gabriel Dunne <gdunne@quilime.com>
2020-07-03 19:29:26 -0700

    Returning response code

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ server.use(cors(corsOptions))
 
 server.post('/upload', upload)
 
+server.get('/', function(req, res){
+   res.send("<pre>200 OK");
+})
+
 server.listen(8000, () => {
   console.log('Server started!')
 })
