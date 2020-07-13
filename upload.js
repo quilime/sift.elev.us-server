2ba52944e560ea0b14da34a57a2859c8fb9915fe
Gabriel Dunne <gdunne@quilime.com>
2020-07-13 11:21:14 -0700

    Iterating upload directories to not max out fs limits

--- upload.js
+++ upload.js
@@ -1,8 +1,10 @@
 const app = require('./app.js');
 const fs = require('fs');
+const path = require('path');
 const shortid = require('shortid');
 const IncomingForm = require('formidable').IncomingForm
 const ExifImage = require('exif').ExifImage;
+const mkdirp = require('mkdirp');
 
 const allowableTypes = {
   'image/jpeg' : 'jpg',
@@ -58,9 +60,34 @@ app.post('/upload', function(req, res) {
         return fileData;
       }, errHandler)
 
+      // then manage filesystem
+      .then(function(fileData){
+        var iter = true;
+        var i = 1;
+
+        do {
+          fileData.destDir = process.env.STATIC_DIR + i + '/';
+          if (fs.existsSync(fileData.destDir) && fs.readdirSync(fileData.destDir).length < process.env.MAX_FILES) {
+            console.log('folder exists and is good');
+            return fileData;
+          } else {
+            console.log('iterating because folder is full');
+            i++;
+            fileData.destDir = process.env.STATIC_DIR + i + '/';
+            fs.mkdirSync(fileData.destDir, { recursive: true }, (err) => {
+              console.log('create new folder');
+              iter = false;
+              return fileData;
+              if (err) throw err;
+            });            
+          }
+        }
+        while(iter);
+      })
+
       // then move static asset
       .then(function(fileData) {
-        fileData.localPath = process.env.STATIC_DIR + fileData.uniqueFilename;        
+        fileData.localPath = fileData.destDir + fileData.uniqueFilename;
         fs.copyFile(fileData.file.path, fileData.localPath, (error) => {
           if (error) {
             throw error;
