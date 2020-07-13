341c7d321f24a80c83c0d9a6d7f740b16cb1ccc2
Gabriel Dunne <gdunne@quilime.com>
2020-07-12 22:41:53 -0700

    Adding shortid and getting upload promise chain working

--- upload.js
+++ upload.js
@@ -1,18 +1,78 @@
 const app = require('./app.js');
+const fs = require('fs');
+const shortid = require('shortid');
 const IncomingForm = require('formidable').IncomingForm
+const ExifImage = require('exif').ExifImage;
+
+const allowableTypes = {
+  'image/jpeg' : 'jpg',
+  'image/jpg' : 'jpg',
+  'image/png' : 'png',
+  'image/gif' : 'gif',
+}
 
 app.post('/upload', function(req, res) {
-  var form = new IncomingForm()
+  var form = new IncomingForm();
+  form.parse(req);
+
+  var fileData = {};
 
   form.on('file', (field, file) => {
-    console.log(file.name);
-    console.log(file.path);
-    // Do something with the file
-    // e.g. save it to the database
-    // you can access it using file.path
-  })
+    
+    fileData.file = file;
+
+    const errHandler = function(err) {
+      console.log(err);
+    };
+
+    const parseEXIF = (filePath) => new Promise(function(resolve, reject) {
+      try {
+        new ExifImage({ image : filePath }, function (error, exifData) {
+          resolve(exifData);
+        });
+      } catch (error) {
+        reject(error);
+      }
+    });
+
+    // parse exif data
+    parseEXIF(file.path).then(function(exif) {
+        fileData.exif = exif;
+        return fileData;
+      }, errHandler)
+
+      // then generate filename
+      .then(function(fileData){
+        if (allowableTypes.hasOwnProperty(fileData.file.type)) {
+          fileData.uniqueFilename = 
+            shortid.generate() + '_' + Date.now() + '.' + allowableTypes[fileData.file.type];
+          return fileData;
+        } else {
+          throw new Error('Filetype "' + fileData.file.type + '" is not allowed.'); 
+        }
+      }, errHandler)
+
+      // then insert to DB
+      .then(function(fileData){
+        console.log('upload to database');
+        return fileData;
+      }, errHandler)
+
+      // then move static asset
+      .then(function(fileData){
+        fs.rename(fileData.file.path, './public/static/' + fileData.uniqueFilename, (error) => {
+          if (error) {
+            throw error;
+          }
+          console.log('File moved!');
+          return true;
+        });
+      }, errHandler)
+
+  });
+  
   form.on('end', () => {
-    res.json()
-  })
-  form.parse(req)
+    console.log('form end');
+    res.json();
+  });
 });
